13b9e446c67f6a2682b26ac0e59b91089a3edaad
jianghu <806224416@qq.com>
2019-12-09 09:07:47 +0800

    第三次修改：完成商品信息的增删改查和spu销售商品的信息保存 第二次修改：完善订单查询地址功能

---
+++ gmall-manage-web/src/test/java/com/business/gmall/manage/GmallManageWebApplicationTests.java
@@ -0,0 +1,19 @@
+package com.business.gmall.manage;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runner.Runner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+//@RunWith(Runner.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GmallManageWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+
+	}
+
+}
